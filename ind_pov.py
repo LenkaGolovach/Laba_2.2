1ed44d37920e404f75bbbf2dda0b7344fdf1992d
LenkaGolovach <karamba290@gmail.com>
2022-05-30 17:40:25 +0300

    Задание повышенной сложности

--- ind_pov.py
+++ ind_pov.py
@@ -5,12 +5,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
-    n = int(input("Введите кол-во чисел в n - "))
-    a = []
-    for i in range(n + 1):
-        a.append(i)
+import math
+import sys
 
-x = int(input("Введите значение х - "))
+EPS = 1e-10
+if __name__ == '__main__':
+    x = float(input("Value of x? "))
+    if x == 0:
+        print("Illegal value of x", file=sys.stderr)
+        exit(1)
+    n = float(input("Value of n -> "))
+    q = (x / 2) ** n
 
+    a = x
+    S, k = a, 1
+    while math.fabs(a) > EPS:
+        a *= (4 * k * math.factorial(k + 1 + n) + 4 * math.factorial(k + 1 + n)) / math.factorial(
+            k - 1) * math.factorial(k + n) * (x ** 2)
+        S += a
+        k += 1
+    print(int(S))
 
